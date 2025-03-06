3811e36ffbc1c30744beb9ba509f27dace901572
Jairo Guerrero <softwavecr@hotmail.com>
2025-03-06 18:30:50 -0500

    first commit

---
+++ backend/microservice1.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/microservice1', methods=['GET'])
+def microservice1():
+    return jsonify(["Hello", "from", "Microservice1"])
+
+if __name__ == '__main__':
+    app.run(port=5000)
---
+++ backend/microservice2.py
@@ -0,0 +1,10 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/microservice2', methods=['GET'])
+def microservice2():
+    return jsonify(["Greetings", "from", "Microservice2"])
+
+if __name__ == '__main__':
+    app.run(port=5001)
